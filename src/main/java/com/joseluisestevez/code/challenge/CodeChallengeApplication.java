b0a1293a5c855817c7fec749462655da004f1e14
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2019-12-05 00:30:34 +0100

    New springboot project

---
+++ src/main/java/com/joseluisestevez/code/challenge/CodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.code.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengeApplication.class, args);
+	}
+
+}
