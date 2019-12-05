0c787e125dea62c1a727e0c471ec8f2199c08705
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2019-12-05 01:34:31 +0100

    Add sample data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO accounts (instrument_number, balance) VALUES('ES9800035314907032954976', 10000.00);
+INSERT INTO accounts (instrument_number, balance) VALUES('ES8030187592819840053937', 23911.99);
+INSERT INTO accounts (instrument_number, balance) VALUES('ES2131348352335125364892', 99999.00);
+INSERT INTO accounts (instrument_number, balance) VALUES('ES6120374915613565136989', 10000.00);
+
+INSERT INTO transactions (reference, account_iban, tx_date, amount, base_amount, fee, description, movement_type, transaction_status, create_at) VALUES ('12345A', 'ES9800035314907032954976', NOW(), 10000.00, 10000.00, 0, 'First deposit', 'DEPOSIT', 'ENDING', NOW());
+INSERT INTO transactions (reference, account_iban, tx_date, amount, base_amount, fee, description, movement_type, transaction_status, create_at) VALUES ('12345A', 'ES8030187592819840053937', NOW(), 23911.99, 23911.99, 0, 'First deposit', 'DEPOSIT', 'ENDING', NOW());
+INSERT INTO transactions (reference, account_iban, tx_date, amount, base_amount, fee, description, movement_type, transaction_status, create_at) VALUES ('12345A', 'ES2131348352335125364892', NOW(), 99999.00, 99999.00, 0, 'First deposit', 'DEPOSIT', 'ENDING', NOW());
+INSERT INTO transactions (reference, account_iban, tx_date, amount, base_amount, fee, description, movement_type, transaction_status, create_at) VALUES ('12345A', 'ES6120374915613565136989', NOW(), 10000.00, 10000.00, 0, 'First deposit', 'DEPOSIT', 'ENDING', NOW());
