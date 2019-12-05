652b62934fb769b9aed63f8324899b9b14d18896
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2019-12-05 01:32:57 +0100

    FIX Id in models

--- src/main/java/com/joseluisestevez/code/challenge/db/model/Account.java
+++ src/main/java/com/joseluisestevez/code/challenge/db/model/Account.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -14,7 +15,7 @@ import javax.persistence.Table;
 public class Account implements Serializable {
 
     private static final long serialVersionUID = -7531087040316251108L;
-
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
--- src/main/java/com/joseluisestevez/code/challenge/db/model/Transaction.java
+++ src/main/java/com/joseluisestevez/code/challenge/db/model/Transaction.java
@@ -10,6 +10,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -22,6 +23,7 @@ public class Transaction implements Serializable {
 
     private static final long serialVersionUID = 2055535404445476401L;
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
